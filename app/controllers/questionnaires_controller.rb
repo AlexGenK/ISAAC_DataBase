3b5a826f6574e1e7f1abe2b1dbfc17ca858f7b74
Oleksii Kozhyn <suburbakskin@gmail.com>
2017-02-09 23:19:48 +0200

    install devise gem

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,5 +1,6 @@
 class QuestionnairesController < ApplicationController
   before_action :set_questionnaire, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:index]
 
   # GET /questionnaires
   # GET /questionnaires.json
