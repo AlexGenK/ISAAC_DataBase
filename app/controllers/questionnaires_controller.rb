9a396cf963f396604abf72a9d4875e28b623ee49
Oleksii Kozhyn <suburbakskin@gmail.com>
2017-06-15 22:51:04 +0300

    add pagination

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -8,7 +8,7 @@ class QuestionnairesController < ApplicationController
   # GET /questionnaires.json
   def index
     params[:search]='' if params[:commit]=='Показать все'
-    @questionnaires = Questionnaire.where('fio LIKE ? AND user LIKE ?', "%#{params[:search]}%", @current_user_email).order(:fio)
+    @questionnaires = Questionnaire.where('fio LIKE ? AND user LIKE ?', "%#{params[:search]}%", @current_user_email).order(:fio).paginate(:page => params[:page], :per_page => 5)
     @count = @questionnaires.count
   end
 
@@ -32,6 +32,7 @@ class QuestionnairesController < ApplicationController
   def create
     @questionnaire = Questionnaire.new(questionnaire_params)
     @questionnaire.user = @current_user_email
+    # flash[:alert] = "----------> #{@questionnaire.is_name_unique} <-----------"
 
     respond_to do |format|
       if @questionnaire.save
@@ -85,7 +86,8 @@ class QuestionnairesController < ApplicationController
                                             :b_1, :b_2, :b_3, :b_4_01, :b_4_02, :b_4_03, :b_4_04, :b_4_05, :b_4_06, :b_4_07, :b_4_08, :b_4_09, :b_4_10, :b_4_11, :b_4_12, :b_5, :b_6, 
                                             :c_1, :c_2, :c_3, :c_4, :c_5, :c_6, :c_7, 
                                             :d_1_1, :d_1_2, :d_1_3, :d_1_4, :d_1_5, :d_1_6, :d_1_7, :d_1_8, :d_1_9, :d_1_10, :d_1_11, :d_1_12, :d_1_13, :d_1_14, :d_1_15, :d_1_16, :d_1_17, :d_1_18, :d_1_19, :d_1_20, :d_1_20_description, :d_1_0, :d_2_1, :d_2_2, :d_2_3, :d_2_4, :d_2_5, :d_2_6, :d_2_7, :d_2_8, :d_2_9, :d_2_10, :d_2_11, :d_2_12, :d_2_13, :d_2_14, :d_2_15, :d_2_16, :d_2_17, :d_2_18, :d_2_19, :d_2_20, :d_2_0, :d_3,
-                                            :dop_1, :dop_2_1, :dop_2_2, :dop_2_3, :dop_2_4, :dop_2_5, :dop_3, :dop_4, :dop_5, :dop_6_1, :dop_6_2, :dop_6_3, :dop_6_4, :dop_6_5, :dop_7_1, :dop_7_2, :dop_7_3, :dop_7_4, :dop_7_5, :dop_8_1, :dop_8_2, :dop_8_3, :dop_8_4, :dop_8_5, :dop_9_1, :dop_9_2, :dop_9_3, :dop_9_4, :dop_10_1, :dop_10_2, :dop_10_3, :dop_10_4, :dop_10_5, :dop_10_6, :dop_11, :dop_12, :dop_13, :dop_14, :dop_15)
+                                            :dop_1, :dop_2_1, :dop_2_2, :dop_2_3, :dop_2_4, :dop_2_5, :dop_3, :dop_4, :dop_5, :dop_6_1, :dop_6_2, :dop_6_3, :dop_6_4, :dop_6_5, :dop_7_1, :dop_7_2, :dop_7_3, :dop_7_4, :dop_7_5, :dop_8_1, :dop_8_2, :dop_8_3, :dop_8_4, :dop_8_5, :dop_9_1, :dop_9_2, :dop_9_3, :dop_9_4, :dop_10_1, :dop_10_2, :dop_10_3, :dop_10_4, :dop_10_5, :dop_10_6, :dop_11, :dop_12, :dop_13, :dop_14, :dop_15,
+                                            :page)
     end
 
     def invalid_questionnaires
